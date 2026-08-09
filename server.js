441d757480c353c1e17c3eaae87fc2672460597d
Mustavojj <mostavojj@gmail.com>
2026-08-09 13:04:53 +0300

    Reduce request limit from 10000 to 100

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 10000,
+    max: 100,
     message: { error: 'Too many requests, please try again later.' },
     standardHeaders: true,
     legacyHeaders: false
