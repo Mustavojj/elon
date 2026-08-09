a602ddbfbccc4c3a4a0f499939157d99341f616c
Mustavojj <mostavojj@gmail.com>
2026-08-09 12:55:18 +0300

    Increase rate limit maximum to 10000 requests

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 10000,
     message: { error: 'Too many requests, please try again later.' },
     standardHeaders: true,
     legacyHeaders: false
